9c09b419952caaa364bc309cb4e231018b37f75b
Vahid Mavaji <mavaji@gmail.com>
2018-05-19 22:39:25 +0430

    add swagger support

--- src/main/kotlin/ir/mavaji/parsmorph/ParsMorphApplication.kt
+++ src/main/kotlin/ir/mavaji/parsmorph/ParsMorphApplication.kt
@@ -2,6 +2,13 @@ package ir.mavaji.parsmorph
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 /**
  * @author Vahid Mavaji
@@ -11,4 +18,17 @@ class ParsMorphApplication
 
 fun main(args: Array<String>) {
     runApplication<ParsMorphApplication>(*args)
+}
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
 }
--- src/main/kotlin/ir/mavaji/parsmorph/controller/MorphologicalAnalyserController.kt
+++ src/main/kotlin/ir/mavaji/parsmorph/controller/MorphologicalAnalyserController.kt
@@ -4,10 +4,7 @@ import ir.mavaji.parsmorph.model.Possibility
 import ir.mavaji.parsmorph.service.MorphologicalAnalyser
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.inject.Inject
 
 /**
@@ -18,6 +15,7 @@ import javax.inject.Inject
 class MorphologicalAnalyserController(@Inject private val morphologicalAnalyser: MorphologicalAnalyser) {
 
     @GetMapping("/")
+    @ResponseBody
     fun analyse(@RequestParam("q") q: String): ResponseEntity<List<Possibility>> {
         val possibilities = morphologicalAnalyser.analyse(q)
 
